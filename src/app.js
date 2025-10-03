cd5ae2a64278f91923ce20a5ac9a8fe2af7db083
Cande <candelariaponcedeleon@outlook.com>
2025-10-03 20:11:51 -0300

    Testing

--- src/app.js
+++ src/app.js
@@ -6,13 +6,12 @@ const express = require('express');
 const connectMongoDB = require('./repositories/mongo_client.js');
 console.log("¿Qué importé?", connectMongoDB);
 
-
-const authMiddleware = require('./middleware/auth_middleware.js');
-
 const signupRouter = require('./routes/signup_router');
 const loginRouter = require('./routes/login_router');
 const servicesRouter = require('./routes/services_router');
 const serviceTypeRouter = require('./routes/serviceType_router');
+const authMiddleware = require('./middleware/auth_middleware.js');
+
 
 const app = express();
 app.use(express.json());
--- src/repositories/mongo_client.js
+++ src/repositories/mongo_client.js
@@ -36,15 +36,32 @@
 
 const mongoose = require('mongoose');
 require('dotenv').config();
+
 async function connectMongoDB() {
-    console.log("Conectando a:", process.env.MONGO_URI);
+    const connectionURL = process.env.MONGO_URI_LOCAL;
+    const dbName = process.env.MONGO_TODOS_DB_NAME;
+
+    console.log("Connected to:", connectionURL, "DB:", dbName);
+
+    if (!connectionURL) {
+        throw new Error('MONGO_DB_HOST not defined');
+    }
+
     try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("Conexión exitosa");
+        await mongoose.connect(connectionURL, {
+            dbName: dbName,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        console.log("Success");
+        mongoose.connection.on('connected', () => console.log('MongoDB connected'));
+        mongoose.connection.on('error', (err) => console.log('MongoDB error', err));
     } catch (err) {
-        console.log("Error conectando", err);
+        console.error("Error connecting", err);
     }
 }
+
 module.exports =  connectMongoDB ;
 
 
